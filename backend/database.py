daa86deabba731bb5e490d04f144048b8d37d69a
Sacksons <vamosakor@gmail.com>
2026-03-15 14:00:39 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -53,18 +53,4 @@ def get_db():
         db.rollback()
         raise
     finally:
-        db.close()    pass
-
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    except Exception:
-        db.rollback()
-        raise
-    finally:
         db.close()
