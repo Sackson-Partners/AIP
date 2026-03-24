469b6916c1ca9933c158af85c23b16bd4a8d16e7
Sacksons <vamosakor@gmail.com>
2026-03-24 22:51:42 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 
 from backend.database import engine
-from backend.models import Base
+from backend.database import Base
 from backend.routers.analytics import router as analytics_router
 from backend.routers.airtable import router as airtable_router
 from backend.routers.auth import router as auth_router
